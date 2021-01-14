1f1417a56d3df6f657c17bbfcb3f5619c9048586
yagnesh-bhalala <yagnesh.bhalala@gmail.com>
2021-01-14 23:13:06 +0530

    test

--- database/migrations/2021_01_02_063606_create_custom_all_tables.php
+++ database/migrations/2021_01_02_063606_create_custom_all_tables.php
@@ -13,6 +13,7 @@ class CreateCustomAllTables extends Migration
      */
     public function up()
     {
+        Schema::dropIfExists('player');
         DB::statement("CREATE TABLE `player` (`id` bigint(20) NOT NULL AUTO_INCREMENT,`user_id` bigint(20) NOT NULL,`player_name` varchar(55) NOT NULL,`player_code` varchar(55) NOT NULL,`opening_balance` float DEFAULT NULL,`player_commision_percentage` float DEFAULT NULL,`third_party_code` varchar(25) DEFAULT NULL,`third_party_percentage` float DEFAULT NULL,`is_flat_commision` tinyint(2) NOT NULL DEFAULT 0,`reached_limit` int(11) DEFAULT NULL,`created_by` bigint(20) NOT NULL,`created_at` timestamp NULL DEFAULT NULL,`updated_at` timestamp NULL DEFAULT NULL,PRIMARY KEY (`id`),KEY `user_id` (`user_id`) ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8");
     }
 
