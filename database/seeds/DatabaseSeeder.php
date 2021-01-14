fa8cbb5d71f6e053b598a6586a8fd20d98f13d13
yagnesh-bhalala <yagnesh.bhalala@gmail.com>
2021-01-14 23:20:11 +0530

    test

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         DB::statement('SET FOREIGN_KEY_CHECKS=0;');
-        DB::statement('TRUNCATE `migrations`;TRUNCATE `model_has_permissions`;TRUNCATE `model_has_roles`;TRUNCATE `password_resets`;TRUNCATE `permissions`;TRUNCATE `player`;TRUNCATE `roles`;TRUNCATE `role_has_permissions`;TRUNCATE `users`;');
+        DB::statement('TRUNCATE `migrations`;TRUNCATE `password_resets`;TRUNCATE `permissions`;TRUNCATE `player`;TRUNCATE `roles`;TRUNCATE `role_has_permissions`;TRUNCATE `users`;TRUNCATE `model_has_permissions`;TRUNCATE `model_has_roles`;');
         DB::statement('SET FOREIGN_KEY_CHECKS=1;');
         $this->call(PermissionSeed::class);
         $this->call(RoleSeed::class);
